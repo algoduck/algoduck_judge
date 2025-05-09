1b6508c0e43c1a204cc79913083329bb36b09149
jwelyl <cork2586@naver.com>
2025-05-09 19:18:51 +0900

    [bugfix] S3 Bucket명 환경변수명 수정

--- app/judge_core.py
+++ app/judge_core.py
@@ -1,3 +1,4 @@
+from app.util.testcase_loader import ensure_testcases_cached
 from fastapi import HTTPException
 import os
 import re
@@ -29,7 +30,7 @@ def judge_submission(req: SubmissionRequest) -> SubmissionResponse:
         raise HTTPException(status_code=400, detail="Only Java (language=1) is supported")
 
     TESTCASE_BASE_PATH = Path(os.getenv("TESTCASE_BASE_PATH", "/default/path"))
-    S3_BUCKET_NAME = os.getenv("TESTCASE_S3_BUCKET_NAME")
+    TESTCASE_S3_BUCKET_URL = os.getenv("TESTCASE_S3_BUCKET_NAME")
     logger.info("TESTCASE_BASE_PATH loaded: %s", TESTCASE_BASE_PATH)
     logger.info("TESTCASE_S3_BUCKET_URL loaded: %s", TESTCASE_S3_BUCKET_URL)
 
@@ -38,7 +39,7 @@ def judge_submission(req: SubmissionRequest) -> SubmissionResponse:
     problem_dir = ensure_testcases_cached(
         problem_id=req.problemId,
         base_path=TESTCASE_BASE_PATH,
-        bucket_name=S3_BUCKET_NAME
+        bucket_name=TESTCASE_S3_BUCKET_URL
     )
     input_files = sorted(problem_dir.glob("input*"), key=sort_key)
     output_files = sorted(problem_dir.glob("output*"), key=sort_key)
